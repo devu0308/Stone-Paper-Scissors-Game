dc0e9030b06d24116a28ba563451316879d86d07
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-02-27 13:49:54 +0530

    Game Completed

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib> 
 using namespace std;
 
 class Game{
@@ -6,18 +7,66 @@ private:
     int playerChoice;
     int computerChoice;
 public:
-    Game(){} // Krina
+    Game(){
+        srand(time(0));
+    }
 
-    void getPlayerChoice(){}  // Takshay
+    void getPlayerChoice(){
+        cout<<"\nChoose the option:\n";
+        cout<<" 1.Stone\n 2.Paper\n 3.Scissors\n";
+        cout<<"Enter Your Choice(1-3): ";
+        cin>>playerChoice;
 
-    void getComputerChoice(){}  // Shubham
+        if(playerChoice<1 or playerChoice>3){
+            cout<<"Please Choose Between 1-3"<<endl;
+        }
+    }
 
-    void determineWinner(){} // Jadeja & Kamil
+    void getComputerChoice(){
+        computerChoice = rand()%3+1;
+    }
 
-    string choiceToString(int choice){} // Mann & Harsh
+    string choiceToString(int choice){
+        switch(choice){
+            case 1:
+                return "Stone";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+            default:
+                return "Unknown";
+        }
+    }
+
+    void determineWinner(){
+        cout<<"\nYou Choose: "<<choiceToString(playerChoice)<<endl;
+        cout<<"Computer Choose: "<<choiceToString(computerChoice)<<endl;
+
+        if(playerChoice == computerChoice){
+            cout<<"Game is Draw"<<endl;
+        }else if((playerChoice == 1 and computerChoice == 3) or
+                (playerChoice == 2 and computerChoice == 1) or
+                (playerChoice == 3 and computerChoice == 2)){
+            cout<<endl<<"Congratulations You Win!🥳"<<endl<<endl;
+        }else{
+            cout<<endl<<"Computer Win Better Luck Next Time!🥺"<<endl<<endl;
+        }
+    }
 };
 
 int main() {
-    cout << "Hello, World!" << endl;
+    char playAgain;
+    do{
+        Game g1;
+        g1.getPlayerChoice();
+        g1.getComputerChoice();
+        g1.determineWinner();
+
+        cout<<"Do you want to play again?(y/n): ";
+        cin>>playAgain;
+    }while(playAgain == 'Y' or playAgain == 'y');
+
+    cout<<"Thanks for Playing.."<<endl;
     return 0;
 }
